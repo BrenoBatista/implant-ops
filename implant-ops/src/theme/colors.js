dd50dc0b385fa688671950b9c432745c9e354d57
Breno Nunes Batista <brenonunesbatista@hotmail.com>
2026-03-20 19:01:28 -0300

    initial configs

---
+++ implant-ops/src/theme/colors.js
@@ -0,0 +1,26 @@
+export const Colors = {
+    light: {
+        background: '#F8F9FA',
+        card: '#FFFFFF',
+        text: '#212529',
+        subtext: '#6C757D',
+        border: '#E0E0E0',
+        primary: '#007BFF',   // Ongoing
+        warning: '#FFC107',   // Waiting
+        danger: '#DC3545',    // overdue/ask
+        success: '#28A745',   // Finished
+    },
+    
+    dark: {
+        background: '#121212',
+        card: '#1E1E1E',
+        text: '#E9ECEF',
+        subtext: '#ADB5BD',
+        border: '#333333',
+        primary: '#3793FF',
+        warning: '#FFD54F',
+        danger: '#E4606D',
+        success: '#48BB78',
+  }
+}
+
